3ecb1becf2aa40b93fd09683d32477a64b27d912
Jordan Price <jprice@izeni.com>
2018-09-03 22:39:56 -0600

    Undo last commit

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./manage.py migrate
 ./manage.py collectstatic --noinput
-sudo -u www-data gunicorn -b 0.0.0.0:8000 --workers=4 punkweb.wsgi
+gunicorn -b 0.0.0.0:8000 --workers=4 punkweb.wsgi
