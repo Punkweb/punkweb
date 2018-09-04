0e732e7ec5a2d2737dd710b39e0a8302fce10fce
Jordan Price <jprice@izeni.com>
2018-09-03 22:37:16 -0600

    start command now runs as www-data

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./manage.py migrate
 ./manage.py collectstatic --noinput
-gunicorn -b 0.0.0.0:8000 --workers=4 punkweb.wsgi
+sudo -u www-data gunicorn -b 0.0.0.0:8000 --workers=4 punkweb.wsgi
