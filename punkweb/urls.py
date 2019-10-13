1489953312e09dcdbff9ab980839321313ec2905
Jordan Price <jprice@izeni.com>
2019-10-13 15:30:08 -0600

    Add api for music section

--- punkweb/urls.py
+++ punkweb/urls.py
@@ -16,11 +16,19 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+from rest_framework.routers import DefaultRouter
 
 from punkweb import settings
 from punkweb import views
+from apps.music import api as music_api
 
 app_name = "punkweb"
+
+router = DefaultRouter()
+router.register(r'artists', music_api.ArtistViewSet, base_name='artists')
+router.register(r'albums', music_api.AlbumViewSet, base_name='albums')
+router.register(r'audio', music_api.AudioViewSet, base_name='audio')
+router.register(r'artist_events', music_api.ArtistEventViewSet, base_name='artist_events')
 
 urlpatterns = [
     # Package urls
@@ -30,6 +38,7 @@ urlpatterns = [
     url(r"^board/api/", include("punkweb_boards.rest.urls")),
     url(r"^captcha/", include("captcha.urls")),
     # Site urls
+    url(r'^api/', include(router.urls)),
     url(r"^$", views.index_view, name="index"),
     url(r"^music/", include("apps.music.urls")),
     url(r"^game/$", views.shietyshooter_view, name="game"),
