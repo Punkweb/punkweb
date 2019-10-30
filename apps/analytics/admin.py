cf115354ce484ef388de16c19b5e3dd14a962ce4
Jordan Price <jprice@izeni.com>
2019-10-30 16:40:34 -0600

    Occurred at on admin analytics

--- apps/analytics/admin.py
+++ apps/analytics/admin.py
@@ -3,7 +3,7 @@ from . import models
 
 
 class AnalyticsEventAdmin(admin.ModelAdmin):
-    list_display = ("category", "action", "label", )
+    list_display = ("category", "action", "label", "occurred_at", )
 
 
 admin.site.register(models.AnalyticsEvent, AnalyticsEventAdmin)
